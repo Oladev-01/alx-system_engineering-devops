624f9b5551733a2c6e9fb78f8e36f837db9f2cf1
olalekan <lekanmojibola@gmail.com>
2024-05-21 18:39:58 +0100

    puppet autmomated script for installing flask in the environment

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
     }
 
 exec { 'flask_installation':
-    command => '/usr/bin/pip3 install flask==2.1.0',
+    command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==0.16.1',
     path    => ['/usr/bin', '/usr/sbin'],
     unless  => '/usr/bin/pip3 show flask | grep -q -i "version: 2.1.0"',
     require => Package['python3-pip']}
