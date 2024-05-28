61427e351210a17132301517a9b010088a109b48
olalekan <lekanmojibola@gmail.com>
2024-05-28 13:53:48 +0100

    puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Define the SSH configuration using Puppet
 file { '/etc/ssh/ssh_config':
     ensure  => present,
-    content => template('./ssh_config_temp'),
+    content => template('/home/oladev-01/alx-system_engineering-devops/0x0B-ssh/ssh_config_temp.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0640',}
