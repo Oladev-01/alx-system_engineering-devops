a596603b689975b5293468a691b298edaf17beb2
olalekan <lekanmojibola@gmail.com>
2024-06-18 11:07:38 +0100

    custom response header

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,62 @@
 # Setup New Ubuntu server with nginx
-
+#update the server
 exec { 'update system':
     command => '/usr/bin/apt-get update',
 }
-
+# ensure Nginx is installed
 package { 'nginx':
     ensure  => 'installed',
     require => Exec['update system']
 }
-
+# landing page
 file {'/var/www/html/index.html':
-    content => 'Hello World!'
+    content => 'Hello World!',
+    require => Package['nginx'],
+}
+# custom page
+file { '/var/www/html/custom_404.txt':
+    ensure  => file,
+    content => "Ceci n'est pas une page",
+    require => Package['nginx'],
+}
+# making a redirection
+exec { 'redirect_me':
+  command  => 'sed -i "/server_name _;/a    location /redirect_me {\n        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n    }" /etc/nginx/sites-available/default',
+  unless   => 'grep -q "location /redirect_me" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require  => Package['nginx'],
 }
 
-exec {'redirect_me':
-    command  => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
+# custom error page
+exec { 'custom 404':
+  command  => 'sed -i "/^\s*location \/ {$/a     error_page 404 /custom_404.txt;\n    location = /custom_404.txt {\\n        internal;\\n    }" /etc/nginx/sites-available/default',
+  unless   => 'grep -q "error_page 404" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require  => Package['nginx'], # Ensure Nginx is installed before running the sed command
+}
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  require => Package['nginx'],
 }
 
+# Remove existing symlink if it exists
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'absent',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx']
+}
+
+# Create symlink to enable the site
+file { '/etc/nginx/sites-enabled/default-link':
+  ensure  => 'link',
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx']
+}
+# enable and restart nginx
 service {'nginx':
-    ensure  => running,
-    require => Package['nginx']
+    ensure    => running,
+    enable    => true,
+    require   => [Package['nginx'], File['/etc/nginx/sites-enabled/default-link']],
+    subscribe => [File['/var/www/html/index.html'], File['/var/www/html/custom_404.txt'], Exec['redirect_me'], Exec['custom 404'],]
 }
---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,23 @@
+### 0x0F-load_balancer
+Project Overview
+This project, provided by ALX, focuses on the concepts and practical implementation of load balancing in web infrastructure. Load balancing is a crucial technique used to distribute incoming network traffic across multiple servers to ensure no single server becomes overwhelmed, which helps improve the availability and reliability of applications.
+
+## Learning Objectives
+This project, provided by ALX, focuses on the concepts and practical implementation and algorithms of load balancing in web infrastructure. Load balancing is a crucial technique used to distribute incoming network traffic across multiple servers to ensure no single server becomes overwhelmed, which helps improve the availability and reliability of applications.
+
+## Learning Objectives
+1. By the end of this project, you should be able to:
+
+2. Understand the concept of load balancing and its importance.
+3. Configure Nginx as a reverse proxy.
+4. Set up a simple load balancer using Nginx.
+5. Implement custom response headers to identify which server handled the request.
+
+## Project Requirements
+* Prerequisites
+* Basic understanding of Nginx configuration.
+* Familiarity with server setup and administration.
+* Basic knowledge of Puppet for automation.
+
+## License
+This repo and its project folder is under Mit License.
