d3fc3f50e19da7dd232fd962479ad0ed92c47e32
olalekan <lekanmojibola@gmail.com>
2024-06-20 11:55:09 +0100

    configured the env to make nginx listen on port 80

--- 0x0E-web_stack_debugging_1/test.sh
+++ 0x0E-web_stack_debugging_1/test.sh
@@ -2,9 +2,14 @@
 # Check if the script is run as root
 
 # Find the process using port 80 and stop it
-if service nginx status | grep -q "active (running)"; then
-  echo "Nginx is now running."
+echo "Checking for processes using port 80..."
+PID=$(netstat -tulnp | grep :80 | awk '{ split($7, parts, "/"); print parts[1]}')
+
+if [ -n"$PID" ]; then
+  echo "Found process using port 80: PID $PID. Stopping it..."
+  kill $PID
 else
-  echo "Failed to start Nginx. Please check the logs for details."
-  exit 1
+  echo "No process found using port 80."
 fi
+service nginx start
+echo "started nginx"
