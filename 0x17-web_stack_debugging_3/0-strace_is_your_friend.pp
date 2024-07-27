cfca2fa2acd5db8686f453ea1baccdf27eb13fd0
olalekan <lekanmojibola@gmail.com>
2024-07-27 08:03:58 +0100

    fixed the bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
 # this puppet script aims to use strace to debug server error
 
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure => 'file',
-  source => 'puppet:///modules/apache/000-default.conf',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  notify => Service['apache2'],
-}
-
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
+exec {'fix-apache2':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
